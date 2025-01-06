d1ff745e14f9f8f6e66b9432a697a4f95818eab5
Nigel Horne <njh@bandsman.co.uk>
2025-01-06 11:05:29 -0500

    Added line chart support

--- lib/HTML/D3.pm
+++ lib/HTML/D3.pm
@@ -44,7 +44,7 @@ our $VERSION = '0.02';
         ['Product C', 200]
     ];
 
-    $html = $chart->render_bar_chart($data);
+    $html = $chart->render_line_chart($data);
     print $html;
 
 =head1 DESCRIPTION
@@ -58,7 +58,8 @@ using D3.js. The module generates HTML and JavaScript code to render the chart i
 
     my $chart = HTML::D3->new(%args);
 
-Creates a new HTML::D3 object. Accepts the following optional arguments:
+Creates a new HTML::D3 object.
+Accepts the following optional arguments:
 
 =over 4
 
@@ -102,7 +103,7 @@ Returns a string containing the HTML and JavaScript code for the chart.
 
 # Method to render a bar chart with given data
 sub render_bar_chart {
-    my ($self, $data, %options) = @_;
+    my ($self, $data) = @_;
 
     # Validate input data to ensure it is an array of arrays
     die 'Data must be an array of arrays' unless ref($data) eq 'ARRAY';
@@ -163,6 +164,106 @@ sub render_bar_chart {
             .call(d3.axisLeft(y));
 
         // Add the x-axis with labels rotated for better readability
+        chart.append("g")
+            .attr("transform", `translate(0,\${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "rotate(-45)")
+            .style("text-anchor", "end");
+    </script>
+</body>
+</html>
+HTML
+
+    return $html;
+}
+
+=head2 render_line_chart
+
+    my $html = $chart->render_line_chart($data);
+
+Generates HTML and JavaScript code to render a line chart. Accepts the following arguments:
+
+=over 4
+
+=item * C<$data> - An array reference containing data points. Each data point should
+be an array reference with two elements: the label (string) and the value (numeric).
+
+=back
+
+Returns a string containing the HTML and JavaScript code for the chart.
+
+=cut
+
+sub render_line_chart {
+    my ($self, $data) = @_;
+
+    # Validate input data
+    die 'Data must be an array of arrays' unless ref($data) eq 'ARRAY';
+
+    # Generate JSON for data
+    my $json_data = JSON::encode_json([
+        map { { label => $_->[0], value => $_->[1] } } @$data
+    ]);
+
+    # Generate HTML and D3.js code
+    my $html = <<"HTML";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$self->{title}</title>
+    <script src="https://d3js.org/d3.v7.min.js"></script>
+</head>
+<body>
+    <h1 style="text-align: center;">$self->{title}</h1>
+    <svg id="chart" width="$self->{width}" height="$self->{height}" style="border: 1px solid black;"></svg>
+    <script>
+        const data = $json_data;
+
+        const svg = d3.select("#chart");
+        const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+        const width = $self->{width} - margin.left - margin.right;
+        const height = $self->{height} - margin.top - margin.bottom;
+
+        const x = d3.scalePoint()
+            .domain(data.map(d => d.label))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.value)])
+            .nice()
+            .range([height, 0]);
+
+        const chart = svg.append("g")
+            .attr("transform", `translate(\${margin.left},\${margin.top})`);
+
+        // Draw line
+        const line = d3.line()
+            .x(d => x(d.label))
+            .y(d => y(d.value));
+
+        chart.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 2)
+            .attr("d", line);
+
+        // Add points to the line
+        chart.selectAll("circle")
+            .data(data)
+            .join("circle")
+            .attr("cx", d => x(d.label))
+            .attr("cy", d => y(d.value))
+            .attr("r", 4)
+            .attr("fill", "steelblue");
+
+        // Add axes
+        chart.append("g")
+            .call(d3.axisLeft(y));
+
         chart.append("g")
             .attr("transform", `translate(0,\${height})`)
             .call(d3.axisBottom(x))
