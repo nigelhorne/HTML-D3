62de4cf2475e76e9758ebe6aa1a91e7f416db6e9
Nigel Horne <njh@bandsman.co.uk>
2026-08-27 13:30:33 -0400

    Release 0.08

--- lib/HTML/D3.pm
+++ lib/HTML/D3.pm
@@ -17,11 +17,11 @@ HTML::D3 - A simple Perl module for generating charts using D3.js.
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
